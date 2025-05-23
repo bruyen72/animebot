8572eba7a7da58ed1c32aa203dad71937361bc6d
bruyen72 <166867926+bruyen72@users.noreply.github.com>
2025-05-23 11:58:25 -0400

    Update config.js

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ mongoose.set('strictQuery', true);
 
 let gg = process.env.MODS;
 if (!gg) {
-  gg = "94774516277";   // You can Change this number //
+  gg = "5565984660212";   // You can Change this number //
 }
 
 // -------------------------------------------------------------- //
